40796f7cc7785ef95e8bcc34b2ad0b5d6f430067
Nagata <ronaldoyutakanagata@gmail.com>
2018-04-22 00:15:43 -0300

    New changes

--- ParalelDevelopment/Task3/gamelife.c
+++ ParalelDevelopment/Task3/gamelife.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 /*
  * Create an array given the the length
  */
 int *allocIntArray(int pLength)
 {
-    int *array;
-    
-    array = malloc(sizeof(int)*(pLength*pLength));
-    
-    return array;
+    return malloc(sizeof(int)*(pLength*pLength));
 }
 
 /*
@@ -70,25 +67,47 @@ int nextState(int *pArray,int i, int j,  int pLength)
     return next;
 }
 
+int nextStateINJ(int *pArray,int i, int j,  int pLength)
+{
+    int up=0, upright=0, right=0, rightdown=0, down=0, downleft=0, left=0, leftup=0, sum=0, next=0;
+    int inj = i*pLength + j;
+    
+    up = pArray[inj - pLength];
+    upright = pArray[inj - pLength + 1];
+    right = pArray[inj + 1];
+    rightdown = pArray[inj + pLength + 1];
+    down = pArray[inj + pLength];
+    downleft = pArray[inj + pLength - 1];
+    left = pArray[inj - 1];
+    leftup = pArray[inj - pLength - 1];
+    
+    sum = up + upright + right + rightdown + down + downleft + left + leftup;
+    
+    if(sum==3)
+    {
+        next = 1;
+    }
+    else if((pArray[pLength*i + j]) == 1 && (sum<2 || sum>3))
+    {
+        next = 0;
+    }
+    
+    return next;
+}
+
 void transferArray(int *pArrayA, int *pArrayB, int pLength)
 {
-    int i, j;
+    int i;
     
-    for (i = 0; i<pLength; i++)
+    for (i = 0; i<(pLength*pLength); i++)
     {
-        for(j=0; j<pLength; j++)
-        {
-            pArrayA[pLength*i + j] = pArrayB[pLength*i + j];
-        }
+        *pArrayA++ = *pArrayB++;
     }
 }
 
 void generateNextPopulation(int *pBoard, int *pNextBoard, int pLength, int pGenerations)
 {
     int i, j, g;
-    
-    temp = initArray(pLength);
-    fillArray(temp, pLength, 0);
     
     printf("###  First Generation ### \n\n");
     printArray(pBoard, pLength);
@@ -99,7 +118,7 @@ void generateNextPopulation(int *pBoard, int *pNextBoard, int pLength, int pGene
         {
             for (j=1; j< pLength-1; j++)
             {
-                pNextBoard[pLength*i + j] = nextState(pBoard, i, j, pLength);
+                pNextBoard[pLength*i + j] = nextStateINJ(pBoard, i, j, pLength);
             }
         }
         
@@ -121,21 +140,35 @@ void initArray(int *pBoard, int pLength)
     pBoard[3*pLength + 3] = 1;
 }
 
+void initRandomArray(int *pArray, int pLength)
+{
+    int i, j;
+    
+    for (i = 0; i<pLength; i++)
+    {
+        for(j=0; j<pLength; j++)
+        {
+            pArray[pLength*i + j] = random()%2;
+        }
+    }
+}
+
 int main(void)
 {
-    int *board, *nextBoard, tamanho, generations;
-    tamanho  = 100;
-    generations = 10;
+    int *board, *nextBoard, length, generations;
+    length  = 10;
+    generations = 6;
+    srandom(time(NULL));
+    board = allocIntArray(length);
+    nextBoard = allocIntArray(length);
     
-    board = allocIntArray(tamanho);
-    nextBoard = allocIntArray(tamanho);
+    fillArray(board, length, 0);
+    fillArray(nextBoard, length, 0);
     
-    fillArray(board, tamanho, 0);
-    fillArray(nextBoard, tamanho, 0);
+    initArray(board, length);
+    //initRandomArray(board, length);
     
-    initArray(board, tamanho);
-    
-    generateNextPopulation(board, nextBoard, tamanho, generations);
+    generateNextPopulation(board, nextBoard, length, generations);
     
     free(board);
     free(nextBoard);
